21f96bdab203c5d93f2a408e29cc27b1e13ec4d0
Dimas Anjasmara <Dimas.anjasmara@gmail.com>
2017-07-27 15:50:03 +0700

    Tugas java 3

---
+++ src/tugas3java3/LatihanP4Komponen.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas3java3;
+
+/**
+ *
+ * @author INDOPORCELAIN
+ */
+public class LatihanP4Komponen {
+     public static void main(String[] args) {
+        // TODO code application logic here
+         LatihanTugas3 L = new LatihanTugas3();
+         L.setVisible(true);
+        
+}
+    
+}
---
+++ src/tugas3java3/Tugas3Java3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas3java3;
+
+/**
+ *
+ * @author INDOPORCELAIN
+ */
+public class Tugas3Java3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tugas3 T = new Tugas3();
+        T.setVisible(true);
+    }
+}
+
+
